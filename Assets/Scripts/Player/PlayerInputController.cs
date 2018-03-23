cc712f5afa43b599af6b0834205562d07dada7a5
Eris Koleszar <eris.koleszar@gmail.com>
2018-03-23 15:24:52 -0500

    Ready PlayerInputController for the start of Lesson 3

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -12,10 +12,6 @@ public class PlayerInputController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Tilling, tilling, tilling!");
-        }
         HandleMovementInput();
     }
 
