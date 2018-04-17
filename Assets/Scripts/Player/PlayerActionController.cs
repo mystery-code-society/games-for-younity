367e06b6813a97631bfe054f788169b6d098e4bd
Eris Koleszar <eris.koleszar@gmail.com>
2018-04-16 22:18:56 -0500

    Tilling in front of the Player

--- Assets/Scripts/Player/PlayerActionController.cs
+++ Assets/Scripts/Player/PlayerActionController.cs
@@ -4,18 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class PlayerActionController : MonoBehaviour
+public class PlayerActionController : CardinalMovementHandler
 {
     [SerializeField] private Tilemap _soilMap;
-    [SerializeField] private Transform _feetPosition;
+    [SerializeField] private Transform _feet;
+    private Vector3 _offset;
+
+    public override void HandleMovement(CardinalDirection direction, float xMovement, float yMovement)
+    {
+        switch(direction)
+        {
+            case CardinalDirection.North:
+                _offset = Vector3.up;
+                break;
+            case CardinalDirection.East:
+                _offset = Vector3.right;
+                break;
+            case CardinalDirection.South:
+                _offset = Vector3.down;
+                break;
+            case CardinalDirection.West:
+                _offset = Vector3.left;
+                break;
+            default:
+                throw new System.ArgumentException("No case defined in HandleMovement() for " + direction);
+        }
+    }
 
     public void TillTheSoil()
     {
-        Vector3Int cellPlayerIsStandingIn = _soilMap.WorldToCell(_feetPosition.position);
-        NaturalGround groundTile = _soilMap.GetTile<NaturalGround>(cellPlayerIsStandingIn);
+        Vector3Int cellToTill = _soilMap.WorldToCell(_feet.position + _offset);
+        NaturalGround groundTile = _soilMap.GetTile<NaturalGround>(cellToTill);
         if(groundTile != null)
         {
-            groundTile.Till(cellPlayerIsStandingIn, _soilMap);
+            groundTile.Till(cellToTill, _soilMap);
         }
     }
 }
