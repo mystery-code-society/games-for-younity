934b0f770f266399d2770ac6c285860ed138db9a
Eris Koleszar <eris.koleszar@gmail.com>
2018-05-02 21:27:58 -0500

    Add the PlayerToolbox, FarmTool, and Tiller classes. Implement Tiller as FarmTool

---
+++ Assets/Scripts/Player/FarmTool.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class FarmTool : MonoBehaviour
+{
+    public abstract void UseTool(Animator animator, Vector3 targetPosition, System.Action callback);
+}
---
+++ Assets/Scripts/Player/Tiller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Tiller : FarmTool
+{
+    [SerializeField] private Tilemap _soilMap;
+    [SerializeField] private Transform _feet;
+    [SerializeField] private GameObject _tillEffect;
+
+    public override void UseTool(Animator animator, Vector3 targetPosition, System.Action callback)
+    {
+        Vector3Int cellToTill = _soilMap.WorldToCell(targetPosition);
+        NaturalGround groundTile = _soilMap.GetTile<NaturalGround>(cellToTill);
+        if(groundTile != null)
+        {
+            Instantiate(_tillEffect, targetPosition + new Vector3(0f, 0f, -6f), _tillEffect.transform.rotation);
+            groundTile.Till(cellToTill, _soilMap);
+        }
+    }
+}
