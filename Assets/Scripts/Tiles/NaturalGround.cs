863088f75d020cb3ca0d20222928b01030c85095
Eris Koleszar <eris.koleszar@gmail.com>
2018-05-12 12:26:14 -0500

    Creating NaturalGround with Crops

--- Assets/Scripts/Tiles/NaturalGround.cs
+++ Assets/Scripts/Tiles/NaturalGround.cs
@@ -3,26 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(fileName ="Ground", menuName ="Scriptable Objects/Natural Ground")]
+[CreateAssetMenu(fileName = "Ground", menuName = "Scriptable Objects/Natural Ground")]
 public class NaturalGround : Tile
 {
     public Condition MyCondition;
     [SerializeField] private NaturalGround[] _groundAssetsOfOtherConditions;
-    
+    public Crop MyCrop;
+
+    public bool CanPlant
+    {
+        get
+        {
+            return MyCondition == Condition.Tilled;
+        }
+    }
+
     public void Till(Vector3Int coordinates, Tilemap tilemap)
     {
         tilemap.SetTile(coordinates, GroundWithCondition(Condition.Tilled));
     }
 
-    private NaturalGround GroundWithCondition(Condition targetCondition)
+    public void Plant(Crop crop, Vector3Int coordinates, Tilemap tilemap)
     {
-        if(MyCondition == targetCondition)
+        Debug.Log("Being planted with " + crop.CropType + " at coordinates " + coordinates);
+        tilemap.SetTile(coordinates, GroundWithCrop(crop));
+    }
+
+    private NaturalGround GroundWithCrop(Crop targetCrop)
+    {
+        if(MyCrop != null && MyCrop == targetCrop)
         {
             return this;
         }
-        for(int i = 0; i < _groundAssetsOfOtherConditions.Length; i++)
+        for (int i = 0; i < _groundAssetsOfOtherConditions.Length; i++)
         {
-            if(_groundAssetsOfOtherConditions[i].MyCondition == targetCondition)
+            if (_groundAssetsOfOtherConditions[i].MyCrop == targetCrop)
+            {
+                return _groundAssetsOfOtherConditions[i];
+            }
+        }
+        throw new System.Exception("No NaturalGround Tile found with Crop " + targetCrop);
+    }
+
+    private NaturalGround GroundWithCondition(Condition targetCondition)
+    {
+        if (MyCondition == targetCondition)
+        {
+            return this;
+        }
+        for (int i = 0; i < _groundAssetsOfOtherConditions.Length; i++)
+        {
+            if (_groundAssetsOfOtherConditions[i].MyCondition == targetCondition)
             {
                 return _groundAssetsOfOtherConditions[i];
             }
@@ -33,6 +64,7 @@ public class NaturalGround : Tile
     public enum Condition
     {
         Compacted,
-        Tilled
+        Tilled,
+        Seeded
     }
 }
