4426d74ebf874a29e88be04c815ace97e3094dc6
Eris Koleszar <eris.koleszar@gmail.com>
2018-05-11 22:23:19 -0500

    Create the Crop script

---
+++ Assets/Scripts/Player/Tools/Crop.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="Crop", menuName ="Scriptable Objects/Crop")]
+public class Crop : ScriptableObject
+{
+    public string Name;
+    public Sprite Seed;
+    public Sprite[] GrowthStages;
+    public GameObject Fruit;
+}
